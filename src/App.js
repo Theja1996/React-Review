62e87ce60ee5930002b0b18f448676cfdf7cbc19
Theja Rasanjalee <ugthejaugamage@gmail.com>
2020-11-23 09:19:42 +0530

    functional component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+//import Fun from './Components/FunctionalComponent';
+import {Arrow} from './Components/FunctionalComponent'
+
+
+function App() {
+  return (
+    <div className="App">
+     {/* <Fun /> */}
+     <Arrow />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/FunctionalComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+//normal java script funtion calling and it has return key word
+// function Fun(){
+//     return <h1>Stateless Functional Componet</h1>
+// }
+
+export const Arrow =()=>  <h1>Arrow function</h1>
+
+//export default Fun;
